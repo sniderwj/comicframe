e28302a82ae60ba5c5adf347608da87f420450e5
sniderwj <sniderwj@gmail.com>
2022-02-24 20:43:50 -0500

    Update AdminFrame.py

--- AdminFrame.py
+++ AdminFrame.py
@@ -30,7 +30,6 @@ webpageEnd: str = '''
  + '''integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" ''' \
  + '''crossorigin="anonymous"></script>
 <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script>
     function searchBarFilter() {
         var input, filter, ul, li, a, i, txtValue;
@@ -59,8 +58,8 @@ webpageBody: str = '''
 <head><title>Comic Frame Filters</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link href="style.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body class='bg-secondary'>
 <div class='container-sm'>
