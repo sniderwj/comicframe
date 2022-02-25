09142076443feeacfd6a5faaa66a7d09ba4abb06
sniderwj <sniderwj@gmail.com>
2022-02-25 16:15:58 -0500

    style

--- AdminFrame.py
+++ AdminFrame.py
@@ -59,6 +59,7 @@ webpageBody: str = '''
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- CSS only -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body class='bg-secondary'>
