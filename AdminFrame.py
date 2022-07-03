3cb90bd977ec57783a42b125614a0e7c57cca2b7
sniderwj <sniderwj@gmail.com>
2022-07-02 21:23:56 -0400

    favicons!

--- AdminFrame.py
+++ AdminFrame.py
@@ -60,8 +60,11 @@ webpageBody: str = '''
 <html lang='en'>
 <head><title>Comic Frame Filters</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="static/bootstrap.min.css" rel="stylesheet">
-
+<link href="static/bootstrap.min.css" rel="stylesheet">
+<link rel="apple-touch-icon" sizes="180x180" href="static/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="static/favicon-16x16.png">
+<link rel="manifest" href="static/site.webmanifest">
 <!-- CSS only
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
